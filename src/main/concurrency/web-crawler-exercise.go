584fdf7b80815babd09012929ad05e4650da426f
lodarki <lodarki@163.com>
2018-08-26 09:20:14 +0800

    web crawler

--- src/main/concurrency/web-crawler-exercise.go
+++ src/main/concurrency/web-crawler-exercise.go
@@ -2,39 +2,80 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-//在这个练习中，将会使用 Go 的并发特性来并行执行 web 爬虫。
-//修改 Crawl 函数来并行的抓取 URLs，并且保证不重复。
 type Fetcher interface {
 	// Fetch 返回 URL 的 body 内容，并且将在这个页面上找到的 URL 放到一个 slice 中。
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+/*
+
+|
+|
+|
+|
+\/
+
+*/
+var (
+	//  map  存放爬取的url
+	m = make(map[string]int)
+	// 互斥锁
+	l sync.Mutex
+	//   群组等待     当添加的任务没有完成时（done（））， wait（） 会一直等待    三个方法   Add()  Done()  Wait()
+	i sync.WaitGroup
+)
+
+func main() {
+	i.Add(1)
+	Crawl("http://golang.org/", 4, fetcher)
+
+	i.Wait() // 会一直等待直到子线程任务结束
+
+	for k, _ := range m {
+		fmt.Println(k)
+	}
+	fmt.Println("over")
+}
+
 // Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: 并行的抓取 URL。
-	// TODO: 不重复抓取页面。
-	// 下面并没有实现上面两种情况：
+
+	defer i.Done() //  和add相对应
 	if depth <= 0 {
 		return
 	}
-	body, urls, err := fetcher.Fetch(url)
+	_, urls, err := fetcher.Fetch(url)
 	if err != nil {
-		fmt.Println(err)
+		//  fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+	// 存入数据  需要同步锁  因为这是在子线程中
+	l.Lock()
+	if m[url] == 0 { //  还未爬取过
+		m[url]++ // 存入爬取的url  改变对应的标示
+		depth--
+		//      fmt.Printf("found: %s %q\n", url, body)
+		for _, u := range urls {
+			i.Add(1)
+			go Crawl(u, depth, fetcher) // 继续爬取
+		}
 	}
-	return
+	l.Unlock()
+
 }
 
-func main() {
-	Crawl("http://golang.org/", 4, fetcher)
-}
+/*
+/\
+|
+|
+|
+|
 
+
+*/
 // fakeFetcher 是返回若干结果的 Fetcher。
 type fakeFetcher map[string]*fakeResult
 
